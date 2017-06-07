baf366113d387c59df9985a75fbf09c536367c74
Greg Williams <grw300@gmail.com>
2017-06-07 16:23:51 -0500

    Authenticated via IdentityServer4 with OpenID. Specified ports.

--- rivER_web/Controllers/HomeController.cs
+++ rivER_web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 
 namespace rivER_web.Controllers
 {
@@ -25,6 +27,20 @@ namespace rivER_web.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        [Authorize]
+        public IActionResult Secure()
+        {
+            ViewData["Message"] = "Your secure area page.";
+
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            await HttpContext.Authentication.SignOutAsync("oidc");
         }
 
         public IActionResult Error()
